d071ed509dc4a9b5d2984cf65659eba44cf04869
TochaFh <rafahoyos08@gmail.com>
2025-06-05 11:51:12 -0300

    comentários exemplo magic

--- magic_logic/exemplo_cartamagic.py
+++ magic_logic/exemplo_cartamagic.py
@@ -1,7 +1,9 @@
 from carta_magic import CartaMagic, TipoCarta
 from batalha import batalha_criaturas
 
-if __name__ == "__main__":
+def main():
+
+    # teste criando carta aleatória
     carta = CartaMagic(
         nome="Relâmpago",
         custo="R",
@@ -9,6 +11,8 @@ if __name__ == "__main__":
         descricao="Relâmpago causa 3 pontos de dano a qualquer alvo."
     )
     print(carta)
+
+    # criando uma criatura 2/2 com iniciativa
     criatura1 = CartaMagic(
         nome="Urso Cinzento",
         custo="1G",
@@ -19,6 +23,8 @@ if __name__ == "__main__":
         iniciativa=True
     )
     print(criatura1)
+
+    # criando uma criatura 1/1
     criatura2 = CartaMagic(
         nome="Goblin Selvagem",
         custo="R",
@@ -29,4 +35,14 @@ if __name__ == "__main__":
     )
     print(criatura2)
 
-    print(batalha_criaturas(criatura1, [criatura2]))
+    # testando batalha entre as criaturas
+    resumo_batalha = batalha_criaturas(criatura1, [criatura2])
+    print()
+    # TODO: fazer algo a respeito das criaturas que morreram
+    # aqui neste ponto as criaturas que tomaram dano estão com a resitência alterada
+    # (como se ainda não tivesse acabado o turno)
+    # TODO: função de fim de turno que reseta as vidas das criaturas
+
+
+if __name__ == "__main__":
+    main()
