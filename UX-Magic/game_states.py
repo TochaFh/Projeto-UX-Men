93f310bc66ea5aa26a433357a02187f850cae39f
lucasmnd <lucasrodriguesdemendonca@gmail.com>
2025-06-15 16:02:28 -0300

    Criando uma lógica baseada em estados e triggers

---
+++ UX-Magic/game_states.py
@@ -0,0 +1,29 @@
+from enum import Enum
+
+class GameState(Enum):
+    START = (0, 0)
+
+    RDC_1_0 = (1, 0)
+    RDC_1_1 = (1, 1)
+    RDC_1_2 = (1, 2)
+    RDC_1_3 = (1, 3)
+
+    RDC_2_0 = (2, 0)
+    RDC_2_1 = (2, 1)
+    RDC_2_2 = (2, 2)
+    RDC_2_3 = (2, 3)
+
+    NEW_TURN = (3, 0)
+
+    MAIN_FIRST = (4, 1)
+    MAIN_SECOND = (4, 2)
+
+    CONJURANDO_MAGICA = (5, 0)
+
+    ATIVANDO_HABILIDADE = (6, 0)
+
+    DECLARANDO_ATACANTES = (7, 0)
+    CONFIRMANDO_ATACANTE = (7, 1)
+    ADICIONANDO_ATACANTE = (7, 2)
+
+    DECLARANDO_BLOQUEADORES = (8, 0)
--- UX-Magic/magic_logic/jogador.py
+++ UX-Magic/magic_logic/jogador.py
@@ -8,13 +8,16 @@ class IDJogador(Enum):
 @dataclass
 class Jogador:
     ID: IDJogador
-    vida: int
-    terrenos_virados: int
-    terrenos_desvirados: int
-    mana_extra: int
-    criaturas: list
+    vida: int = 20
+    terrenos_virados: int = 0
+    terrenos_desvirados: int = 0
+    mana_extra: int = 0
+    criaturas: list = []
+    cards_hand: list = []
+    cards_bf: list = []
+    cards_grave: list = []
 
     def iniciar_turno(self):
-        self.terrenos_desvirados += self.terrenos_virados
+        self.terrenos_desvirados += self.terrenos_virados + 1
         self.terrenos_virados = 0
         self.mana_extra = 0
