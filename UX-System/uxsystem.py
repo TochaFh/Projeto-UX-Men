479300210c93cd4c417a3be0a9302603c487bcd3
TochaFh <rafahoyos08@gmail.com>
2025-06-15 19:32:43 -0300

    uxsystem agora é uma classe

--- UX-System/uxsystem.py
+++ UX-System/uxsystem.py
@@ -3,75 +3,77 @@ from comunica_rasp import SerialReader
 _ID = {}
 id_count = 0
 
-ON_RFID = []
-ON_LEITURA = []
-ON_B_VERMELHO = []
-ON_B_AZUL = []
 
-# ID detectado no modo PADRÃO
-def _on_rd(rfid):
-    global id_count
-    if not _ID.get(rfid):
-        id_count += 1
-        _ID[rfid] = id_count
-        print(f"Nova carta detectada: {_ID[rfid]}")
+class UXSystem:
+    def __init__(self):
+        self.serial_thread = None
+        self.ON_RFID = []
+        self.ON_LEITURA = []
+        self.ON_B_VERMELHO = []
+        self.ON_B_AZUL = []
 
-    for callback in ON_RFID:
-        try:
-            callback(_ID[rfid])
-        except Exception as e:
-            print(f"** Erro ao chamar callback em _on_rd: {e}")
+    # ID detectado no modo PADRÃO
+    def _on_rd(self, rfid):
+        global id_count
+        if not _ID.get(rfid):
+            id_count += 1
+            _ID[rfid] = id_count
+            print(f"Nova carta detectada: {_ID[rfid]}")
 
-# ID detectado no modo de LEITURA
-def _on_rr(rfid):
-    if _ID.get(rfid):
-        print(f"Leitura de carta: {_ID[rfid]}")
-    
-    for callback in ON_LEITURA:
-        try:
-            callback(_ID.get(rfid, None))
-        except Exception as e:
-            print(f"** Erro ao chamar callback em _on_rr: {e}")
+        for callback in self.ON_RFID:
+            try:
+                callback(_ID[rfid])
+            except Exception as e:
+                print(f"** Erro ao chamar callback em _on_rd: {e}")
 
-# Botão azul pressionado
-def _on_bb():
-    for callback in ON_B_AZUL:
-        try:
-            callback()
-        except Exception as e:
-            print(f"** Erro ao chamar callback em _on_bb: {e}")
+    # ID detectado no modo de LEITURA
+    def _on_rr(self, rfid):
+        if _ID.get(rfid):
+            print(f"Leitura de carta: {_ID[rfid]}")
+        
+        for callback in self.ON_LEITURA:
+            try:
+                callback(_ID.get(rfid, None))
+            except Exception as e:
+                print(f"** Erro ao chamar callback em _on_rr: {e}")
 
-# Botão vermelho pressionado
-def _on_br():
-    for callback in ON_B_VERMELHO:
-        try:
-            callback()
-        except Exception as e:
-            print(f"** Erro ao chamar callback em _on_br: {e}")
+    # Botão azul pressionado
+    def _on_bb(self):
+        for callback in self.ON_B_AZUL:
+            try:
+                callback()
+            except Exception as e:
+                print(f"** Erro ao chamar callback em _on_bb: {e}")
 
-def start():
-    global serial_thread
-    serial_thread = SerialReader(_on_rd, _on_rr, _on_br, _on_bb)
-    serial_thread.start()
+    # Botão vermelho pressionado
+    def _on_br(self):
+        for callback in self.ON_B_VERMELHO:
+            try:
+                callback()
+            except Exception as e:
+                print(f"** Erro ao chamar callback em _on_br: {e}")
 
-def close():
-    global serial_thread
-    serial_thread.stop()
+    def start(self):
+        self.serial_thread = SerialReader(self._on_rd, self._on_rr, self._on_br, self._on_bb)
+        self.serial_thread.start()
+
+    def close(self):
+        self.serial_thread.stop()
 
 def teste():
     from time import sleep
-    start()
+    uxs = UXSystem()
+    uxs.start()
     try:
         print("# UX-System rodando...")
         while True:
             sleep(0.05)
     except KeyboardInterrupt:
-        close()
+        uxs.close()
         print("# UX-System encerrado.")
     except Exception as e:
-        close()
-        print(f"** Erro inesperado: {e}")
-    pass
+        uxs.close()
+        print(f"** Erro inesperado no teste (raiz): {e}")
 
 if __name__ == "__main__":
     teste()
